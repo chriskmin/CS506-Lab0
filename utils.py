35cfe301cd9ea568d84c8e8b0dbe74e9df0e8df2
Chris <chrismin@bu.edu>
2024-09-12 12:42:44 -0400

    implemented cosine similairty

---
+++ utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def dot_product(v1, v2):
+    '''
+    v1 and v2 are vectors of same shape.
+    return the scalar dot product of the two vectors.
+    '''
+    return np.dot(v1, v2)
+
+def cosine_similarity(v1, v2):
+    '''
+    v1 and v2 are vectors of same shape.
+    Return the cosine similarity between the two vectors.
+    '''
+    dot_prod = dot_product(v1, v2)
+
+    norm_v1 = np.linalg.norm(v1)
+    norm_v2 = np.linalg.norm(v2)
+    return dot_prod / (norm_v1 * norm_v2)
+
+def nearest_neighbor(target_vector, vectors):
+    '''
+    target_vector is a vector of shape d.
+    vectors is a matrix of shape N x d.
+    return the row index of the vector in vectors that is closest to 
+    target_vector in terms of cosine similarity.
+    '''
+    best_index = -1
+    best_similarity = -1 
+
+    for i, vector in enumerate(vectors):
+        similarity = cosine_similarity(target_vector, vector)
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_index = i
+    
+    return best_index
